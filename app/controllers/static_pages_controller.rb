925744a77469391b48241bc028ed6735834ded7d
Cliquar <postitherepls@aol.de>
2017-03-22 18:40:07 +0100

    Heroku Mailer added

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,19 @@
 class StaticPagesController < ApplicationController
   def index
   end
-
+  
   def landing_page
     @featured_product = Product.first
     @products = Product.last(3)
+  end
+
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(:from => @email,
+      :to => 'postitherepls@aol.de',
+      :subject => "A new contact form meddage from #{@name}",
+      :body => @message).deliver_now
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
   root 'static_pages#index'
-  get 'contact', to: 'static_pages#contact'
+  post 'static_pages/thank_you'
   post 'static_pages/index'
 
   resources :orders, only: [:index, :show, :create, :destroy]
