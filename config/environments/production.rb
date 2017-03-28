f969e487def52f77c012be9186fc8c343a140817
Cliquar <postitherepls@aol.de>
2017-03-28 16:38:05 +0200

    changed default url again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,14 +84,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  Rails.application.routes.default_url_options[:host] = 'bikenurembergdemo.herokuapp.com'
-
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default :charset => "utf-8"
-
-
+  
   ActionMailer::Base.smtp_settings = {
   :address => 'smtp.sendgrid.net',
   :port => '587',
