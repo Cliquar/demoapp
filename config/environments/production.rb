5de1c7cf46abf85eb5103dfa09c18ee39ef7cd93
Cliquar <postitherepls@aol.de>
2017-03-28 16:56:25 +0200

    changed default mail again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,15 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   
-  
+  ActionMailer::Base.smtp_settings = {
+  :address => 'smtp.sendgrid.net',
+  :port => '587',
+  :authentication => :plain,
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'heroku.com',
+  :enable_starttls_auto => true
+  }
 
-  config.action_mailer.default_url_options = { host: 'https://bikenurembergdemo.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'bikenurembergdemo.herokuapp.com' }
 end
