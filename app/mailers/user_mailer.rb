13bc6ebd55003f4839a8f213e2e86ebe21ba2a10
Cliquar <postitherepls@aol.de>
2017-04-11 13:36:51 +0200

    added welcome email on sign up

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,9 @@ class UserMailer < ApplicationMailer
       :to => "postitherepls@aol.de",
       :subject => "A new contact form message from #{name}")
   end
+
+  def welcome_email(user)
+    mail(:to => user.email,
+      :subject => "Welcome to BikeNuremberg!")
+  end
 end
