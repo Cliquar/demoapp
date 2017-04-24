0ea911add7bf61806a8985442ecefb16c122c188
Cliquar <postitherepls@aol.de>
2017-04-24 15:09:30 +0200

    deleted ruby in development gemfile

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+
+describe Product do
+    
+  before do
+    @product = Product.create!(name: "race bike")
+    @user = User.create!(email: "test@test.com", password: "123456")
+    @product.comments.create!(rating: 1, user: @user, body: "Awful bike!")
+    @product.comments.create!(rating: 3, user: @user, body: "Meh bike!")
+    @product.comments.create!(rating: 5, user: @user, body: "Nice bike!")
+  end
+
+  context "when the product has comments" do
+    it "returns the average of all comments" do
+
+      expect(@product.average_rating).to eq 3
+    end
+
+
+    it "is not valid" do
+      expect(Product.new(description: "Nice bike")).to be_valid
+    end
+  end
+end
