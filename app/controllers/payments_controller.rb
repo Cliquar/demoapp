c11514a9ff3bdf8af5c262ce9cc08d8734b0b346
Cliquar <postitherepls@aol.de>
2017-04-27 19:38:11 +0200

    added redirect to paid

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -14,6 +14,7 @@ class PaymentsController < ApplicationController
     if charge.paid
       Order.create(product_id: @product.id, user_id: @user.id, total: @product.price)
       flash[:success] = "Your payment was processed successfully"  
+      redirect_to product_path(@product)
 
     end
 
