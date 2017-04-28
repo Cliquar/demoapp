5d5a755d089207dc8c4976b006e027a3494725e5
Cliquar <postitherepls@aol.de>
2017-04-28 16:17:38 +0200

    added post route for payments/create

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   root 'static_pages#landing_page'
   post 'static_pages/thank_you'
   post 'static_pages/index'
+  post 'payments/create'
 
   resources :orders, only: [:index, :show, :create, :destroy]
 
